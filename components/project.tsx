1c68e9e12156aafc9bc93f19af08f86186ae340b
Agusilva7 <niupi7374@gmail.com>
2023-11-02 15:57:58 -0300

    feacture responsive

--- components/project.tsx
+++ components/project.tsx
@@ -25,7 +25,7 @@ const opacityProgess=useTransform(scrollYProgress,[0,1],[0.6,1])
     <motion.div style={{scale:scaleProgess,opacity:opacityProgess}} className=" group mb-3 sm:mb-8 last:mb-0 ">
 
     <section  ref={ref} 
-      className=" bg-gray-100 max-w[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
+      className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
       >
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{title}</h3>
@@ -47,7 +47,7 @@ const opacityProgess=useTransform(scrollYProgress,[0,1],[0.6,1])
             src={imageUrl}
             alt="Project I worked on"
             quality={95}
-            className="absolute hidden sm:block top-8 right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-hover:sacale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:rotate-2  group-even:group-hover:-translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:-right-[initial]  group-even:-left-40"
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-hover:sacale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:rotate-2  group-even:group-hover:-translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:-right-[initial]  group-even:-left-40"
             />
         </div>
         </section>
